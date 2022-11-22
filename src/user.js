3e9b8f4ec8bc3f4a53f0fc606ca1e99e3481a1c5
Your Name <you@example.com>
2022-11-22 11:43:36 +0530

    change

--- src/user.js
+++ src/user.js
@@ -66,7 +66,7 @@ function User() {
 
   return (
     <div>
-      <Usernavbar />
+      <Navbar />
       <div className="form-group col-xs-4">
         <h4>Select Order Of Pricing</h4>
         <select className="form-control" onChange={(e) => setval(e.target.value)}>
